52d652d739badcb908eb6c4fbf8fbeef9f3b42dc
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-11 22:59:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -432,3 +432,20 @@ tasButton.addEventListener('click', shout)
 </body>
 
 // app.js
+const button=document.querySelector('button')
+const h1=document.querySelector('h1')
+
+button.addEventListener('click',function(){
+    const newColor=randColor();
+    document.body.style.backgroundColor=newColor;
+    h1.innerText=randColor();
+})
+
+const randColor=()=>{
+    const r=Math.floor(Math.random()*255);
+    const g=Math.floor(Math.random()*255);
+    const b=Math.floor(Math.random()*255);
+
+    return `rgb(${r},${g},${b})`;
+}
+
