4a09ae4d24c9adf96b7737e1ec260e93d5c3dc91
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-21 20:10:18 +0530

    Add files via upload

---
+++ express-js/exp1.js
@@ -0,0 +1,17 @@
+//express js
+//web framework for nodeJs
+//Listening for incoming requests, figuring out what they are asking for,what are the searching for
+//whats the url,q, search data,  heck data in the db and send back to the client
+
+
+//Express helps us\\
+//1.startup a server to listen for requests
+//2. parse incoming requests
+//3.match those requests to particular routes
+//craft our http response and associated content
+
+//Read about Library vs Framework\\
+//when you're using a library,you are in charge!you control the flow of of the app,and you decide when to use the lib
+//lib example: Axios, libs often do smaller things for us or single purpose,like making http requests or changing the colour of the terminal
+//with fw,the control is inverted.the fw is in charge,you're merely a participant
+//fw's ambition is large, creating a web app, a game ex. Express
