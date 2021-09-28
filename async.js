502d1c2a9383199282b4580b61b8d10298c129db
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-28 23:27:11 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,7 @@
+// synchronus vs asynchronus
+// in asynchronus, many things can happen at a same time
+//in synchronus one thing can only happen after a particular thing is done
+//as I know JS is a synchronus single threaded language
+
+//in js we can perform Async functionalities using these words
+//async/await, callback, promises
